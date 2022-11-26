74852854a1d9d1a49bb41d5c7de4a7a2652076a5
CrossStackQ <crossstack@gmail.com>
2022-11-26 21:07:18 +0530

    initial

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -28,7 +28,6 @@ import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 const people = [
     { id: 1, name: 'Wade Cooper' },
     { id: 2, name: 'Arlene Mccoy' },
-    { id: 3, name: 'Devon Webb' },
   ]
   
 
